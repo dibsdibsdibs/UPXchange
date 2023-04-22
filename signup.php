09e66f5a75caaaa4738415a2d7c05ce38ceb7324
Divine Lavente <97289418+dibsdibsdibs@users.noreply.github.com>
2023-04-22 15:13:08 +0800

    Update signup.php

--- signup.php
+++ signup.php
@@ -15,7 +15,7 @@ session_start();
     <!--Header bar section-->
     <div class="top-header">
         <div class = "header-left">
-            <img src = "pics/logo_white.png" height="75">
+            <img src = "pics/logo_white.png" height="50">
             <h1>UP Xchange</h1>
         </div>
         <div class = "header-right">
@@ -26,8 +26,22 @@ session_start();
             <a><img src = "pics/user.png" height="25"></a>
         </div>
     </div>
-    <div id="signup">
+    <div id="signup">   
         <h1>SIGN UP</h1>
+        <div id="signup-content">
+            <div>
+                <h2>Email</h2>
+                <input type=text placeholder = "Type your email">
+            </div>
+            <div>
+                <h2>Password</h2>
+                <input type=text placeholder = "Type your password">
+            </div>
+            <div>
+                <h2>Confirm Password</h2>
+                <input type=text placeholder = "Confirm your password">
+            </div>
+        </div>
     </div>
 </body>
 </html>
