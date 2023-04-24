382598d18dbf69bc0a115aa7827cc9f0ef993797
Divine <divinelavente@gmail.com>
2023-04-24 20:48:12 +0800

    Sign Up Page and General Design CSS
    Updated sign up page and general design css. Added new monteserrat font.

--- signup.php
+++ signup.php
@@ -14,11 +14,11 @@ session_start();
 <body class="bg">
     <!--Header bar section-->
     <div class="top-header">
-        <div class = "header-left">
+        <div id = "header-left">
             <img src = "pics/logo_white.png" height="50">
             <h1>UP Xchange</h1>
         </div>
-        <div class = "header-right">
+        <div id = "header-right">
             <a><img src = "pics/magnifying glass.png" height="25"></a>
             <a><img src = "pics/plus.png" height="25"></a>
             <a><img src = "pics/categories.png" height="25"></a>
@@ -26,21 +26,36 @@ session_start();
             <a><img src = "pics/user.png" height="25"></a>
         </div>
     </div>
+    <!-- Sign Up section-->
     <div id="signup">   
+        <br>
         <h1>SIGN UP</h1>
         <div id="signup-content">
             <div>
-                <h2>Email</h2>
+                <h3>Email</h3>
                 <input type=text placeholder = "Type your email">
-            </div>
-            <div>
-                <h2>Password</h2>
+                <h3>Password</h3>
                 <input type=text placeholder = "Type your password">
-            </div>
-            <div>
-                <h2>Confirm Password</h2>
+                <h3>Confirm Password</h3>
                 <input type=text placeholder = "Confirm your password">
             </div>
+            <br><br>
+            <button type="submit" id="create">CREATE ACCOUNT</button>
+            <br>
+            <div>
+                <p>Already have an account?</p>
+            </div>
+            <button type="submit" id="login">LOGIN</button>
+        </div>
+    </div>
+    <!-- Footer bar section -->
+    <div class="bottom-footer">
+        <div id="footer-left">
+            <img src = "pics/logo_white.png" height="50">
+            <h1>UP Xchange</h1>
+        </div>
+        <div id="footer-right">
+            <p>© 2023 UP Xchange. Up Xchange is a trademark brand owned by UP Xchange. A Philippine-registered company. All other trademarks are owned by their respective owners.</p>
         </div>
     </div>
 </body>
