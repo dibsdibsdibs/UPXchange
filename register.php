552e02c92877fb3ed2ba48d1a0dba1f2d06d6e28
Jewynah <jhjaronay@up.edu.ph>
2023-05-09 23:18:45 +0800

    b

--- register.php
+++ register.php
@@ -22,15 +22,16 @@
             $error = "Passwords don't match!";
             break;
         case $password == $repassword:
-            $uppercase = preg_match('@[A-Z]@', $password);
-            $lowercase = preg_match('@[a-z]@', $password);
-            $number    = preg_match('@[0-9]@', $password);
-            $specialChars = preg_match('@[^\w]@', $password);
+             $uppercase = preg_match('@[A-Z]@', $password);
+             $lowercase = preg_match('@[a-z]@', $password);
+             $number    = preg_match('@[0-9]@', $password);
+             $specialChars = preg_match('@[^\w]@', $password);
         
             if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 8) {
                 $error = 'Password should be at least 8 characters in length and should include at least: 1 uppercase letter, 1 number, and 1 special character.';
                 break;
             }
+            
         default:
             $check_upmail  = $conn -> query("SELECT * FROM users WHERE upmail = '$upmail'");
             if(mysqli_num_rows($check_upmail) >= 1){
