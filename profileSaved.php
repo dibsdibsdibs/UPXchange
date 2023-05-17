2f7e30a03659883f27619e821af7f71c4c02e6fe
Cloydi <ccybiernas@up.edu.ph>
2023-05-17 21:00:35 +0800

    Changed the photo profile and inserted file upload
    However, still not backed to database

--- profileSaved.php
+++ profileSaved.php
@@ -1,44 +1,44 @@
 <?php
-// Start the session
-session_start();
+    // Start the session
+    session_start();
 
-// Check if the user is logged in or redirect to the login page if not
-if (!isset($_SESSION['user_id'])) {
+    // Include the database connection file
+    include 'dbconnector.php';
+
+    // Retrieve the submitted form data
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $course = $_POST['course'];
+    $membership = $_POST['membership'];
+    $year = $_POST['year'];
+    $about = $_POST['about'];
+
+    // Create a database connection
+    $conn = mysqli_connect($hostname, $username, $password, $database);
+    if (!$conn) {
+        // Handle connection error
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    // Prepare the SQL statement
+    $sql = "INSERT INTO user_profiles (first_name, last_name, course, membership, year, about) VALUES (?, ?, ?, ?, ?, ?)";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, "ssssss", $firstName, $lastName, $course, $membership, $year, $about);
+
+    // Execute the statement
+    if (mysqli_stmt_execute($stmt)) {
+        // Profile saved successfully
+        $_SESSION['success_message'] = "Your profile has been saved successfully.";
+    } else {
+        // Profile saving failed
+        $_SESSION['error_message'] = "An error occurred while saving your profile. Please try again.";
+    }
+
+    // Close the statement and database connection
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
+
+    // Redirect the user back to the profile editing page
     header("Location: editProfileFR.php");
     exit;
-}
-
-// Retrieve the submitted form data
-$firstName = $_POST['firstName'];
-$lastName = $_POST['lastName'];
-$course = $_POST['course'];
-$membership = $_POST['membership'];
-$year = $_POST['year'];
-$about = $_POST['about'];
-
-// Perform validation and sanitization on the form data
-// ...
-
-// Store the user profile data in the database
-// Assuming you have a database connection established
-
-// Prepare the SQL statement
-$sql = "INSERT INTO user_profiles (first_name, last_name, course, membership, year, about) VALUES (?, ?, ?, ?, ?, ?)";
-$stmt = $pdo->prepare($sql);
-
-// Bind the values and execute the statement
-$stmt->execute([$firstName, $lastName, $course, $membership, $year, $about]);
-
-// Check if the profile was successfully saved
-if ($stmt->rowCount() > 0) {
-    // Profile saved successfully
-    $_SESSION['success_message'] = "Your profile has been saved successfully.";
-} else {
-    // Profile saving failed
-    $_SESSION['error_message'] = "An error occurred while saving your profile. Please try again.";
-}
-
-// Redirect the user back to the profile editing page
-header("Location: editProfile.php");
-exit;
 ?>
