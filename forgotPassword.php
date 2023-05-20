aab94b78d33701dfab0beb5627c031eb4fb99b1b
Jewynah <jhjaronay@up.edu.ph>
2023-05-20 09:06:58 +0800

    frontend pa lng sng forgot password

--- forgotPassword.php
+++ forgotPassword.php
@@ -9,7 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="styles/general.css" type="text/css" rel="stylesheet">
-    <link href="styles/login.css" type="text/css" rel="stylesheet">
+    <link href="styles/forgotPassword.css" type="text/css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" rel="stylesheet" >
     <link href="pics/logo_white.png" rel="icon">
     <title>Login</title>
@@ -34,7 +34,7 @@
     <div class="container">
         <div id="login">   
             <br>
-            <h1 style="font-size:40px;">LOG IN</h1>
+            <h1 style="font-size:40px;">Forgot Password</h1>
             <form action="log_in.php" method="post">
                 <div id="error">
                 <?php
@@ -44,26 +44,18 @@
                     }
                 ?>
                 </div>
+            <div id="signup-option">
+            <p style="font-size:18px;">Please enter your registered UPmail. We will send a link to reset your password.</p>
+            </div>
             <div id="login-content">
                 <div>
                     <h3>UPmail</h3>
                     <input type=text id="upmail" name="upmail" placeholder = "Type your UPmail" pattern=".+@up\.edu\.ph" title="example@up.edu.ph" maxlength="20">
-                    <h3>Password</h3>
-                    <div class = "password-container">
-                        <input type=password id = "password" name="password" placeholder = "Type your password" maxlength="20" pattern = "(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*?[0-9])(?=.*?[~`!@#$%\^&*()\-_=+[\]{};:\x27.,\x22\\|/?><]).{8,}" title="Must contain at least one number, one symbol, one uppercase and lowercase letter, and at least 8 or more characters" >
-                        <i class="fa-solid fa-eye" id="eye"></i>
-                    </div>
-                </div>
-                <div class="subContainer">
-                    <p class="forgotpassword"><a href="forgotPassword.php" style="font-size:17px;">Forgot Password?</a></p>
                 </div>
                 <br><br>
-                <button type="submit" id="create" style="font-size:18px;">LOG IN</button>
+                <button type="submit" id="create" style="font-size:18px;">Send link</button>
                 <br>
-                <div id="signup-option">
-                    <p style="font-size:18px;">Don't have an account?</p>
-                </div>
-                <button type="submit" id="login" formaction="signup.php" style="font-size:20px;">SIGN UP</button>
+                <button type="submit" id="login" formaction="login.php" style="font-size:20px;">Back to Login</button>
             </div>
         </div>
     </div>
