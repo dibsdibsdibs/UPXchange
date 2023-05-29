e832346728507bd34822262ff7aee31c45d0d3d7
Gabriel Venz Badilles <gvbadilles@up.edu.ph>
2023-05-30 02:13:49 +0800

    Header and footer changes
    Included header and footer changes and changed css

--- addQuestion.php
+++ addQuestion.php
@@ -15,19 +15,7 @@ session_start();
 
 <body class="bg-addQuestion">
     <!--Header bar section-->
-    <div class="top-header">
-        <div id = "header-left">
-            <img src = "pics/logo_white.png" height="50">
-            <h1>UP Xchange</h1>
-        </div>
-        <div id = "header-right">
-            <a><img src = "pics/magnifying glass.png" height="25"></a>
-            <a><img src = "pics/plus.png" height="25"></a>
-            <a><img src = "pics/categories.png" height="25"></a>
-            <a><img src = "pics/bell.png" height="25"></a>
-            <a><img src = "pics/user.png" height="25"></a>
-        </div>
-    </div>
+    <?php include "header.php" ; ?>
     <div id="error-display">
         <div id="error">
             <?php
@@ -56,15 +44,7 @@ session_start();
     <button type="submit" id="post">POST</button>
     </form>
     <!-- Footer bar section -->
-    <div class="bottom-footer">
-        <div id="footer-left">
-            <img src = "pics/logo_white.png" height="50">
-            <h1>UP Xchange</h1>
-        </div>
-        <div id="footer-right">
-            <p>© 2023 UP Xchange. Up Xchange is a trademark brand owned by UP Xchange. A Philippine-registered company. All other trademarks are owned by their respective owners.</p>
-        </div>
-    </div>
+    <?php include "footer.php" ; ?>
     <script src="scripts/addQuestionJS.js" type="text/javascript"></script>
 </body>
 </html>
