2476f0c6a6ada36305a53a5cbf1d6b9a53ed6de7
Jewynah <jhjaronay@up.edu.ph>
2023-05-29 23:26:41 +0800

    editprofile

--- editProfileFR.php
+++ editProfileFR.php
@@ -34,15 +34,10 @@
         </div>
     </div>
     <div class="center">
-        <h2>EDIT PROFILE</h2>
+        <h2 style="font-size:40px;">EDIT PROFILE</h2>
         <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
             <form action="upload.php" method="post" enctype="multipart/form-data">
-                <div class="wrapper">Profile Picture:
-                <input type="file" name="fileToUpload" id="fileToUpload">
-                <br>    
-                <br>
-                <input type="submit" value="Upload Image" name="submit">
-            </div>
+            <h4 style="margin-right: 190px;margin-bottom: 10px;">First Name:</h4>
             <div class="row mt-5">
                 <div class="wrapper">
                     <input type="text" maxlength="30" class="form-control" placeholder="First Name" name="firstName" action = "
@@ -52,6 +47,7 @@
                 ?>">
                 </div>
             </div>
+            <h4 style="margin-right: 190px;margin-bottom: 10px;">Last Name:</h4>
             <div class="row mt-3">
                 <div class="wrapper"><input type="text" maxlength = "30" class="form-control" placeholder="Last Name" action = "
                     <?php     if(!empty($firstName) && !ctype_alpha($firstName)) {
@@ -59,24 +55,49 @@
     } 
                 ?>"></div>
             </div>
+            <h4 style="margin-right: 220px;margin-bottom: 10px;">Course:</h4>
             <div class="row mt-3">
-                <div class="wrapper"><input type="text" maxlength = "30" class="form-control" placeholder="Course" action = "
-                    <?php     if(!empty($firstName) && !ctype_alpha($firstName)) {
-            return "You typed $firstName: please use only alphanumeric characters";
-    } 
-                ?>"></div>
+                <div class="wrapper">                    
+                    <select class = "form-control" name="membership" id="membership">
+                    <option value="select">--select--</option>
+                    <option value="BA in Communication and Media Studies">BA in Communication and Media Studies</option>
+                    <option value="BA Community Development">BA Community Development</option>
+                    <option value="BA History">BA History</option>
+                    <option value="BA Sociology">BA Sociology</option>
+                    <option value="BA in Literature">BA in Literature</option>
+                    <option value="BA in Psychology">BA in Psychology</option>
+                    <option value="BS Biology">BS Biology</option>
+                    <option value="BS Accountancy">BS Accountancy</option>
+                    <option value="BS Applied Mathematics">BS Applied Mathematics</option>
+                    <option value="BS Business Administration (Marketing)">BS Business Administration (Marketing)</option>
+                    <option value="BS Chemical Engineering">BS Chemical Engineering</option>
+                    <option value="BS Chemistry">BS Chemistry</option>
+                    <option value="BS Computer Science">BS Computer Science</option>
+                    <option value="BS Economics
+">BS Economics</option>
+                    <option value="BS Fisheries">BS Fisheries</option>
+                    <option value="BS Food Technology">BS Food Technology</option>
+                    <option value="BS Management">BS Management</option>
+                    <option value="BS Public Health">BS Public Health</option>
+                    <option value="BS Statistics">BS Statistics</option>
+                    </select>
+                </div>
             </div>
+            <h4 style="margin-right: 120px;margin-bottom: 10px;">Student or Faculty?</h4>
             <div class="row mt-3">
                 <div class="wrapper">
                     <select class = "form-control" name="membership" id="membership">
+                    <option value="select">--select--</option>
                     <option value="Faculty">Faculty</option>
                     <option value="Student">Student</option>
                     </select>
                 </div>
             </div>
+            <h4 style="margin-right: 195px;margin-bottom: 10px;">Year Level:</h4>
             <div class="row mt-3">
                 <div class="wrapper">
                     <select class = "form-control" name="membership" id="membership" value="Year">
+                        <option value="select">--select--</option>
                         <option value="1st">First Year</option>
                         <option value="2nd">Second Year</option>
                         <option value="3rd">Third Year</option>
@@ -85,13 +106,19 @@
                     </select>
                 </div>
             </div>
+            <h4 style="margin-right: 230px;margin-bottom: 10px;">About:</h4>
             <div class="row mt-3">
                 <div class="wrapper">
-                    <input type="text" maxlength = "150" class="form-control-about" placeholder="About">
+                    <input type="text" maxlength = "150" class="form-control-about" placeholder="Tell us about yourself...">
                 </div>
-            </div>     
+            </div>  
+            <div class="row mt-3">
+                <div class="wrapper">
+                    <button class="btn btn-primary profile-button" type="submit">Save Profile</button>
+                </div>
+            </div>      
     </div>  
-    <button class="btn btn-primary profile-button" type="submit">Save Profile</button>
+
 </html>
 
 <?php
