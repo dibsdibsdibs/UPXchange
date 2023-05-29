42d12be84bc99b26bcefa7dcdbe1d98706389f74
Gabriel Venz Badilles <gvbadilles@up.edu.ph>
2023-05-30 02:12:21 +0800

    Header and footer changes
    Replaced header and footer code

--- footer.php
+++ footer.php
@@ -1,11 +1,9 @@
-<div id="footer">
-    <div class="wrapper">
-        <div id="footer-left">
-            <img src="pics/logo_white.png" height="50">
-            <h1>UP XChange</h1>
-        </div>
-        <div id="footer-right">
-            <p>© 2023 UP Xchange. Up Xchange is a trademark brand owned by UP Xchange. A Philippine-registered company. All other trademarks are owned by their respective owners.</p>
-        </div>
+<div class="bottom-footer">
+    <div id="footer-left">
+        <img src = "pics/logo_white.png" height="50">
+        <h1>UP Xchange</h1>
+    </div>
+    <div id="footer-right">
+        <p>© 2023 UP Xchange. Up Xchange is a trademark brand owned by UP Xchange. A Philippine-registered company. All other trademarks are owned by their respective owners.</p>
     </div>
 </div>
--- header.php
+++ header.php
@@ -1,17 +1,13 @@
-<div id="header">
-    <div class="wrapper">
-        <a href="#">
-            <img src="pics/logo_white.png" height="50">
-            <h1>UP XChange</h1>
-        </a>
-        <nav>
-            <ul>
-                <li><a href="#"><img src="pics/magnifying glass.png" height="30"></a></li>
-                <li><a href="addQuestion.php"><img src="pics/plus.png" height="30"></a></li>
-                <li><a href="#"><img src="pics/categories.png" height="30"></a></li>
-                <li><a href="#"><img src="pics/bell.png" height="30"></a></li>
-                <li><a href="editProfile.php"><img src="pics/user.png" height="30"></a></li>
-            </ul>
-        </nav>
+<div class="top-header">
+    <div id = "header-left">
+        <img src = "pics/logo_white.png" height="50">
+        <h1>UP Xchange</h1>
+    </div>
+    <div id = "header-right">
+        <a href="#"><img src="pics/magnifying glass.png" height="25"></a>
+        <a href="addQuestion.php"><img src="pics/plus.png" height="25"></a>
+        <a href="#"><img src="pics/categories.png" height="25"></a>
+        <a href="#"><img src="pics/bell.png" height="25"></a>
+        <a href="editProfile.php"><img src="pics/user.png" height="25"></a>
     </div>
 </div>
