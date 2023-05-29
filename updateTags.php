86070f3c3e0206cf498ba0cfb62cfcfda6835bf2
Divine <divinelavente@gmail.com>
2023-05-30 03:19:17 +0800

    Implemented edit question system

---
+++ updateTags.php
@@ -0,0 +1,21 @@
+<?php
+    include 'dbconnector.php';
+    session_start();
+
+    if(isset($_POST['listtags'])){
+        function validate($data){
+            $data = stripslashes($data);
+            return $data;
+        }
+    }
+
+    $tags = $_POST['listtags'];
+    $tag_id = $_SESSION['question_id'];
+    $_SESSION['tags'] = $tags;
+
+    $sql = "UPDATE tags SET tags = '$tags' WHERE tag_id = '$tag_id'";
+
+    if($conn -> query($sql) == TRUE){
+        exit();
+    }
+?>
